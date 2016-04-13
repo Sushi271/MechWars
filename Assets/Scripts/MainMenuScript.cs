90a9c061a3f605bc6115cb997471cd0fa45f9f01
Dolores <dolores.celestial@gmail.com>
2016-04-13 20:31:03 +0200

    tworzenie buttonów w skrepcie. że udaję, że umiem.

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -5,6 +5,7 @@ public class MainMenuScript : MonoBehaviour {
 
 private bool _isFirstMenu = true;
 private bool _isLevelSelectMenu = false;
+private bool _isOptionMenu = false;
 
 	// Use this for initialization
 	void Start () {
@@ -21,6 +22,12 @@ private bool _isLevelSelectMenu = false;
 	}
 	void FirstMenu()
 	{
-		
+		if (_isFirstMenu)
+		{
+			if (GUI.Button(new.Rect(10, Screen.height / 2 - 100, 150, 25) "New Game"))
+			{
+				
+			}
+		}
 	}
 }
