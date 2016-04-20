c84bcffc16341be93928e2fea554474542838c77
Dolores <dolores.celestial@gmail.com>
2016-04-20 13:35:10 +0200

    Dzień i noc w Test i Menu

--- Assets/Scripts/Day_and_night.cs
+++ Assets/Scripts/Day_and_night.cs
@@ -12,5 +12,6 @@ public class Day_and_night : MonoBehaviour {
 	void Update ()
     {
         transform.RotateAround(Vector3.zero, Vector3.right, 10f * Time.deltaTime); //względem pktu 0, w prawo, szybkość
+        transform.LookAt(Vector3.zero);//kierunek patrzenia
 	}
 }
