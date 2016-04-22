bbc059267a28be4c31ce5a03a2faab8f35679dde
Sushi271 <vathintroth@gmail.com>
2016-04-22 22:46:04 +0200

    Camera refactor Camera zoom enabled

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,21 +4,17 @@ namespace MechWars
 {
     public class CameraController : MonoBehaviour
     {
-        public int scrollMargin = 20;
-        public float scrollSpeed = 20;
+        public GameObject handle;
+
+        public int scrollMargin = 15;
+        public float scrollSpeed = 10;
         public bool scrollingEnabled = true;
 
-        public Vector3 Center
-        {
-            get
-            {
-                var v = transform.forward;
-                var p = transform.position;
-                var t = -p.y / v.y;
-                return p + t * v;
-            }
-        }
-
+        float minZoom = 0.25f;
+        float maxZoom = 4;
+        public float zoom = 1;
+        public float zoomFactor = 10;
+        
         void Update()
         {
             if (scrollingEnabled)
@@ -31,21 +27,35 @@ namespace MechWars
                 if (!rect.Contains(mp))
                 {
                     var vel = Vector3.zero;
-                    if (mp.x < rect.xMin) vel -= new Vector3(1, 0, 0);
-                    if (rect.xMax < mp.x) vel += new Vector3(1, 0, 0);
-                    if (mp.y < rect.yMin) vel -= new Vector3(0, 0, 1);
-                    if (rect.yMax < mp.y) vel += new Vector3(0, 0, 1);
-                    vel *= scrollSpeed;
+                    if (mp.x < rect.xMin) vel += new Vector3(-1, 0, 1);
+                    if (rect.xMax < mp.x) vel += new Vector3(1, 0, -1);
+                    if (mp.y < rect.yMin) vel += new Vector3(-1, 0, -1);
+                    if (rect.yMax < mp.y) vel += new Vector3(1, 0, 1);
+                    vel *= scrollSpeed * zoom;
 
-                    transform.localPosition += Time.deltaTime * vel;
+                    handle.transform.localPosition += Time.deltaTime * vel;
                     CorrectCameraPosition();
                 }
+            }
+
+            var mwAxis = Input.GetAxis(Axes.MouseWheel);
+            if (mwAxis != 0)
+            {
+                var dZoom = mwAxis * zoomFactor;
+                var expDZoom = Mathf.Pow(0.9f, dZoom);
+                var newZoom = zoom * expDZoom;
+                if (newZoom > maxZoom) newZoom = maxZoom;
+                if (newZoom < minZoom) newZoom = minZoom;
+                var realExpDZoom = newZoom / zoom;
+                zoom = newZoom;
+
+                handle.transform.localScale = Vector3.one * zoom;
             }
         }
 
         void CorrectCameraPosition()
         {
-            var center = Center;
+            var center = handle.transform.position;
             var glb = Globals.Instance;
 
             var correction = Vector3.zero;
@@ -54,7 +64,7 @@ namespace MechWars
             if (center.z < 0) correction -= new Vector3(0, 0, center.z);
             if (glb.MapHeight - 1 < center.z) correction -= new Vector3(0, 0, center.z - (glb.MapHeight - 1));
 
-            transform.position += correction;
+            handle.transform.position += correction;
         }
     }
 }
