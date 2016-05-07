91c0eb771e98ec2e86f9258dd8f999873555ff85
Sushi271 <vathintroth@gmail.com>
2016-05-07 21:24:59 +0200

    Turret fires ahead of moving unit, hitting it >;->

--- Assets/Scripts/MapElements/Attacks/ProjectileAttack.cs
+++ Assets/Scripts/MapElements/Attacks/ProjectileAttack.cs
@@ -1,4 +1,5 @@
-﻿using MechWars.MapElements.Statistics;
+﻿using MechWars.MapElements.Jobs;
+using MechWars.MapElements.Statistics;
 using MechWars.Utils;
 using UnityEngine;
 
@@ -31,6 +32,55 @@ namespace MechWars.MapElements.Attacks
         }
 
         Vector3 CalculateStartingVelocity(Projectile projectile)
+        {
+            var velocity = CalculateWithGravity(projectile);
+
+            // TODO: it's wrong, it needs to do it in 3-fucking-D
+            if (Target.JobQueue.CurrentJob is MoveJob)
+                velocity = CalculateWithEscapingTarget(projectile, velocity);
+
+            return velocity;
+        }
+
+        Vector3 CalculateWithEscapingTarget(Projectile projectile, Vector3 velocity)
+        {
+            var moveJob = (MoveJob)Target.JobQueue.CurrentJob;
+
+            var A = Attacker.Coords;
+            var B = Target.Coords;
+            var AB = B - A;
+            var ABLen = AB.magnitude;
+            var ABLenSq = ABLen * ABLen;
+
+            var VALen = velocity.AsHorizontalVector2().magnitude;
+            var VALenSq = VALen * VALen;
+
+            var VB = moveJob.Velocity;
+            var VBLen = VB.magnitude;
+            var VBLenSq = VBLen * VBLen;
+
+            var VBDotAB = Vector2.Dot(VB, AB);
+
+            var alpha = ABLenSq;
+            var beta = 2 * VBDotAB;
+            var gamma = VBLenSq - VALenSq;
+            var Delta = beta * beta - 4 * alpha * gamma;
+
+            if (Delta >= 0)
+            {
+                var sqrtDelta = Mathf.Sqrt(Delta);
+                var reciprocT1 = (-beta - sqrtDelta) / (2 * alpha);
+                var reciprocT2 = (-beta + sqrtDelta) / (2 * alpha);
+                var reciprocT = reciprocT1 >= 0 ? reciprocT1 : reciprocT2;
+                var t = 1 / reciprocT;
+
+                var vel = AB / t + VB;
+                velocity = new Vector3(vel.x, velocity.y, vel.y);
+            }
+            return velocity;
+        }
+
+        Vector3 CalculateWithGravity(Projectile projectile)
         {
             var startPos = projectile.transform.position;
             var targetPos = Target.Coords.AsHorizontalVector3() + Target.yToAim * Vector3.up;
--- Assets/Scripts/MapElements/Jobs/MoveJob.cs
+++ Assets/Scripts/MapElements/Jobs/MoveJob.cs
@@ -8,27 +8,34 @@ namespace MechWars.MapElements.Jobs
     {
         public Unit Unit { get; private set; }
         public IVector2 Delta { get; private set; }
+        public Vector2 Direction { get; private set; }
         public IVector2 Destination { get; private set; }
+        
+        public float Speed { get; private set; }
+        public Vector2 Velocity { get; private set; }
 
         public MoveJob(Unit unit, IVector2 delta)
             : base(unit)
         {
             Unit = unit;
             Delta = delta.Sign();
+            Direction = Delta.Normalized;
             Destination = unit.Coords.Round() + Delta;
+
+            var speedStat = Unit.Stats[StatNames.MovementSpeed];
+            if (speedStat == null)
+                throw new System.Exception(string.Format("Missing {0} Stat in Unit's Stats! (Unit {1})",
+                    StatNames.MovementSpeed, Unit));
+            Speed = speedStat.Value;
+            Velocity = Delta.Normalized * Speed;
         }
 
         protected override void OnUpdate()
         {
-            var speed = Unit.Stats[StatNames.MovementSpeed];
-            if (speed == null)
-                throw new System.Exception(string.Format("Missing {0} Stat in Unit's Stats! (Unit {1})",
-                    StatNames.MovementSpeed, Unit));
-
             var toDest = Destination - Unit.Coords;
-            float dDist = speed.Value * Time.deltaTime;
+            float dDist = Speed * Time.deltaTime;
             if (dDist < toDest.magnitude)
-                Unit.Coords += Delta.Normalized * dDist;
+                Unit.Coords += Direction * dDist;
             else
             {
                 Unit.Coords = Destination;
