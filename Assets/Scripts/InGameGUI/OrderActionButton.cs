0c11d033302866d0de50a090de0acda502bc316b
Dolores <dolores.celestial@gmail.com>
2016-09-03 22:51:00 +0200

    batony cz.2

--- Assets/Scripts/InGameGUI/OrderActionButton.cs
+++ Assets/Scripts/InGameGUI/OrderActionButton.cs
@@ -1,4 +1,6 @@
-﻿using MechWars.MapElements.Orders.Actions;
+﻿using MechWars.MapElements;
+using MechWars.MapElements.Orders.Actions;
+using System.Linq;
 using UnityEngine;
 
 namespace MechWars.InGameGUI
@@ -8,10 +10,33 @@ namespace MechWars.InGameGUI
         // akcja ktora dziala ten button
         public OrderAction orderAction;
 
-        public void OnClick()
+        public void OnClick_ChangeCarriedOrderAction()
         {
-            // ustawiamy ja jako "niesiona" przez myszke
+            // ustawiamy akcje jako "niesiona" przez myszke
             Globals.Spectator.InputController.CarriedOrderAction = orderAction;
+        }
+
+        public void OnClick_ChangeCarriedBuildingOrderAction()
+        {
+            var inputController = Globals.Spectator.InputController;
+            var selected = inputController.SelectionMonitor.SelectedMapElements;
+            // ustawiamy akcje jako "niesiona" przez myszke i tworzymy BuildingShadow oraz ConstructionRange
+            inputController.CarriedOrderAction = orderAction;
+            inputController.CreateShadow((BuildingConstructionOrderAction)orderAction);
+            inputController.CreateConstructionRange((Building)selected.First());
+        }
+
+        public void OnClick_InvokeOrderAction()
+        {
+            var inputController = Globals.Spectator.InputController;
+            var selected = inputController.SelectionMonitor.SelectedMapElements;
+
+            // dla kazdej z zaznaczonych jednostek / dla zaznaczonego budynku
+            foreach (var mapElement in selected)
+            {
+                // wywołujemy akcję od razu
+                orderAction.GiveOrder(mapElement, inputController.OrderActionArgs);
+            }
         }
     }
 }
---
+++ Assets/Scripts/InGameGUI/OrderUtilityButton.cs
@@ -0,0 +1,24 @@
+﻿using MechWars.MapElements;
+using System.Linq;
+using UnityEngine;
+
+namespace MechWars.InGameGUI
+{
+    public class OrderUtilityButton : MonoBehaviour
+    {
+        // Guzik Cancel na ConstYard, Factory i Laboratory
+        public void OnClick_CancelProduction()
+        {
+            var buildings = Globals.Spectator.InputController.SelectionMonitor.SelectedMapElements.OfType<Building>();
+            var bld = buildings.FirstOrDefault();
+
+            if (bld != null)
+                bld.OrderQueue.CancelCurrent();
+        }
+
+        // Guzik Cancel staly
+        public void OnClick_CancelSelection()
+        {
+        }
+    }
+}
